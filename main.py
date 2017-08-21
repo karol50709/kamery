a907880df584f8dd58b34c4e564e59fbe43fafea
Karol <karol50709@gmail.com>
2017-08-21 21:20:39 +0200

    Multi copy

--- main.py
+++ main.py
@@ -7,19 +7,22 @@ matches = []
 for root, dirnames, filenames in os.walk('/home/karol/Pobrane/'):
     for filename in fnmatch.filter(filenames, '*.txt'):
         matches.append(os.path.join(root, filename))
-
 count=len(matches)
 print count
 
 for i in range(len(matches)):
-    print (i," ",matches[i])
+    print i," ",matches[i]
 
-name = input("Podaj numer pliku ktory chcesz skopiowac ")
-print matches[name]
-shutil.copy(matches[name], destination)
-print "zmiana nazwy pliku na .mtxcopy"
-temp=matches[name]+"copy"
-print temp
-os.rename(matches[name],temp)
+name = input("Podaj numery plikow ktore chcesz skopiowac, oddzielaj numery przecinkiem ',' ")
+namestring=str(name)
+tabnum=namestring.split(",")
+for x in tabnum:
+    temps = x.strip("() ")
+    i=int(temps)
+    shutil.copy(matches[i], destination)
+    print "skopiowano plik:     ", matches[i]
+    temp=matches[i]+"copy"
+    os.rename(matches[i], temp)
+    print  "zmieniono nazwe na: ", temp
 
-
+print "Koniec"
